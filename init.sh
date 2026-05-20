24379081ac567907e396cd3b9b1a5d18ea2fde9b
Ubuntu <ubuntu@ip-172-31-82-133.ec2.internal>
2026-05-20 18:55:11 +0000

    init files

---
+++ init.sh
@@ -0,0 +1,6 @@
+sudo apt update # To bring VM snapshot up to date with package versions
+sudo apt install make -y # so we can use makefiles
+sudo apt install python-venv -y 
+### had issues with the Ubuntu OS i used so i did the generic python build instead 
+### so we can create python virtual environments
+sudo apt install tree
---
+++ init_git_creds.sh
@@ -0,0 +1,11 @@
+!#/usr/bin/bash
+
+USER=wqp7qy@virginia.edu
+NAME=abrish2049
+
+git config --global --list
+
+git config --global user.email ${USER} 
+git config --global user.name  ${NAME} 
+
+git config --global --list
